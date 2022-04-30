20607422eaaf7b0aaa5eddbf8ddd273ae0e6d36d
gug903 <91352110+gug903@users.noreply.github.com>
2022-04-30 17:20:44 -0500

    Add files via upload
    bug fixes and added more allergen/preference options

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,16 +1,19 @@
 import re
-
+#allergens / preferences tuple of ingredients to exclude 
 nut_ = ("peanut", "almond", "cashew", "nut")
 dairy_ = ("milk", "cheese", "butter", "dairy")
 gluten_ = ("bread", "gluten")
+vegitarian_=("meat","beef","pork","poultry","chicken","fish")
+#also sets vegitarian and dairy flags on 
+vegan_=("egg","honey","mayo")
 
-
-###################test cases
-# result = shoppingsort([("zx", "mexico", ("tomato", "beef")), ("ax", "mexico", ("tomato", "beef")), ("cx", "mexico", ("bread", "meat"))], "", 0, 0, 0)
-# result = shoppingsort([("burger", "mexico", ("tomato", "beef")), ("burger", "ax", ("tomato", "beef")), ("hot dog", "mexico", ("bread", "meat"))], "burg", 0, 0, 1)
 # sorts a list of data elements in format ((foodname,country,(list of ingredients...)...), search query string, allergens as a 1 or 0 representing true or false,,,
-def shoppingsort(datalist, search, nut, dairy, gluten):
-    datacount = 0
+def shoppingsort(datalist, search, nut, dairy, gluten, vegi, vegan):
+    dataremovelist = []
+    datacount=0
+    if vegan == 1:
+        dairy = 1
+        vegi = 1
     for data in datalist:
         match = 0
         if re.search(r"" + search, data[0], re.IGNORECASE):
@@ -33,19 +36,36 @@ def shoppingsort(datalist, search, nut, dairy, gluten):
                         if re.search(r"" + i, ing, re.IGNORECASE):
                             match = 0
                             break
+            if vegi == 1:
+                for i in vegitarian_:
+                    for ing in data[2]:
+                        print(data[0],i,ing)
+                        if re.search(r"" + i, ing, re.IGNORECASE):
+                            match = 0
+                            break
+            if vegan == 1:
+                for i in vegan_:
+                    for ing in data[2]:
+                        if re.search(r"" + i, ing, re.IGNORECASE):
+                            match = 0
+                            break
         if match == 0:
-            datalist.pop(datacount)
+            dataremovelist.append(datacount)
         datacount += 1
+    adj=0
+    for i in dataremovelist:
+        datalist.pop(i-adj)
+        adj+=1
     datalist.sort()
     return datalist
 
-
-#result = shoppingsort([("Zx", "mexico", ("tomato", "beef")), ("Ax", "mexico", ("tomato", "beef")), ("Cx", "mexico", ("bread", "meat"))], "", 0, 0, 0)
-#result = shoppingsort([("good burger", "mexico", ("tomato", "beef")), ("bad burger", "ax", ("tomato", "beef")), ("hot dog", "mexico", ("bread", "meat"))], "burg", 0, 0, 1)
+#tests
+#result = shoppingsort([("Zx", "mexico", ("tomato", "beef")), ("Ax", "mexico", ("tomato", "beef","meat")), ("Cx", "mexico", ("bread", "meat")),("tx", "mexico", ("tomato","grass"))], "", 0, 0, 0,1,1)
+#result = shoppingsort([("hat","africa",("cloth","dye")),("good burger", "mexico", ("tomato", "beef")), ("bad burger", "ax", ("tomato", "beef", "bread")), ("hot dog", "mexico", ("bread", "meat"))], "burg", 1, 1, 1, 0, 0)
 #print(result)
 
 
-# data=[(foodname,country,(ingredients...)),(),()...]
+#tup=("","","",...)
 def tuptostring(tup):
     ret = ""
     for ing in tup:
@@ -53,11 +73,12 @@ def tuptostring(tup):
 
     return ret
 
-
+# data=[(foodname,country,(ingredients...)),(),()...]
 def datatostring(data):
     ret = ""
     for rec in data:
         ret += rec[0] + " from " + rec[1] + "\n\tIngredients: " + tuptostring(rec[2]) + "\n\n"
     return ret
-
-# print(datatostring([("beef","mexico",("meat", "mystery meat")),("hamburger","USA",("bread","meat","chesse"))]))
+#tests
+#print(datatostring([("beef","mexico",("meat", "mystery meat")),("hamburger","USA",("bread","meat","chesse"))]))
+#print(datatostring(result))
